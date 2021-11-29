9ac8198a010aa201a348593a4f02cef9b8873d32
remorgan <reggie.morgan@viasat.com>
2021-11-29 11:50:45 +0000

    created container deployment scripts

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,1 +1,25 @@
-module.exports = {};
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js", // all built files will use this as a template - (Name + Hash) for caching
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container", // not required but conventional
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`, // use an additional marketing folder to ensure remoteEntry files are uniquely identifiable.
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
